2b6669555d62c716aafeab2536b41cb57f71cfbb
arpitvaghela <arpitvaghela9210@gmail.com>
2021-08-04 21:02:02 +0530

    complex type in recursive serde

--- packages/syft/examples/experimental/module_test.py
+++ packages/syft/examples/experimental/module_test.py
@@ -1,6 +1,12 @@
+import pandas as pd
+
 class A:
-    def __init__(self,n:int):
+    def __init__(self,n:int,data:pd.DataFrame):
         self.n = n
+        self.data = data
     
     def get_n(self)->int:
         return self.n
+    
+    def get_data(self)->pd.DataFrame:
+        return self.data
